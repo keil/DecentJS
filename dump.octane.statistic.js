8a812f344535c6e908eb261ff46d3aa2ed6459f1
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-11-13 18:52:19 +0000

    cync
    git-svn-id: https://proglang/svn/proglang/projects/decentjs/trunk@25912 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ dump.octane.statistic.js
@@ -0,0 +1,108 @@
+./js -f bootstrap.js -f benchmark/octane/octane.js
+
+
+
+*** Include benchmark/octane/richards.js ***
+Richards: 5790
+----
+Score (version 9): 5790
+Statistic: #wrap:164032 #cache miss:12 #decompile:1 #cache hit:164012 #touched:2 
+
+
+
+*** Include benchmark/octane/crypto.js ***
+Crypto: 2810
+----
+Score (version 9): 2810
+Statistic: #wrap:2378498 #cache miss:19 #decompile:1 #cache hit:2378468 #touched:2 
+
+
+
+*** Include benchmark/octane/raytrace.js ***
+RayTrace: 906
+----
+Score (version 9): 906
+Statistic: #wrap:25519236 #cache miss:16 #decompile:1 #cache hit:25519211 #touched:3 
+
+
+
+*** Include benchmark/octane/earley-boyer.js ***
+EarleyBoyer: 1445
+----
+Score (version 9): 1445
+Statistic: #wrap:5491 #cache miss:31 #decompile:1 #cache hit:5443 #touched:8 
+
+
+
+*** Include benchmark/octane/regexp.js ***
+RegExp: 1518
+----
+Score (version 9): 1518
+Statistic: #wrap:131964 #cache miss:14 #decompile:1 #cache hit:131942 #touched:2 
+
+
+
+*** Include benchmark/octane/splay.js ***
+Splay: 1426
+SplayLatency: 4707
+----
+Score (version 9): 2591
+Statistic: #wrap:466852 #cache miss:14 #decompile:1 #cache hit:466829 #touched:2 
+
+
+
+*** Include benchmark/octane/navier-stokes.js ***
+NavierStokes: 2226
+----
+Score (version 9): 2226
+Statistic: #wrap:1850 #cache miss:13 #decompile:1 #cache hit:1829 #touched:2 
+
+
+
+*** Include benchmark/octane/pdfjs.js ***
+PdfJS: 300
+----
+Score (version 9): 300
+Statistic: #wrap:35363530 #cache miss:33 #decompile:1 #cache hit:35363482 #touched:8 
+
+
+
+*** Include benchmark/octane/mandreel.js ***
+Mandreel: 366
+MandreelLatency: 2128
+----
+Score (version 9): 882
+Statistic: #wrap:19958253 #cache miss:27 #decompile:1 #cache hit:19958215 #touched:3 
+
+
+
+*** Include benchmark/octane/gbemu-part1.js,benchmark/octane/gbemu-part2.js ***
+65538
+Gameboy: 13144
+----
+Score (version 9): 13144
+Statistic: #wrap:533175 #cache miss:24 #decompile:1 #cache hit:533102 #touched:2 
+
+
+
+*** Include benchmark/octane/code-load.js ***
+CodeLoad: 7348
+----
+Score (version 9): 7348
+Statistic: #wrap:50821 #cache miss:231 #decompile:1 #cache hit:50179 #touched:2 
+
+
+
+*** Include benchmark/octane/box2d.js ***
+Box2D: 388
+----
+Score (version 9): 388
+Statistic: #wrap:27014006 #cache miss:23 #decompile:1 #cache hit:24876414 #touched:2 
+
+
+
+*** Include benchmark/octane/typescript-input.js,benchmark/octane/typescript-compiler.js,benchmark/octane/typescript.js ***
+Typescript: 4554
+----
+Score (version 9): 4554
+Statistic: #wrap:1114215 #cache miss:18 #decompile:1 #cache hit:1114186 #touched:2 
