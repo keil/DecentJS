659a4044094357890236dc997558d41883f31caa
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-11-20 12:38:52 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/decentjs/trunk@25946 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ demo/fileA.js
@@ -0,0 +1,9 @@
+function __A(x) {
+  return x;
+}
+/*__A__.prototype = {};
+__A__.prototype.toString = function() {
+  return "[[object A]]"
+}*/
+
+var __a = new __A();
---
+++ demo/fileB.js
@@ -0,0 +1,12 @@
+/*
+function __B__() {
+}
+__B__.prototype = Object.create(A.prototype);
+__B__.prototype.toString = function() {
+  return "[[object B]]"
+}
+
+var __a__ = new __A__();
+var __b__ = new __B__();
+var __c__ = new __B__();
+*/
