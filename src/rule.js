9c03328958f4233cbbb2dcefba79bea90d46a76c
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-12-13 08:22:23 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/decentjs/trunk@26024 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ src/rule.js
@@ -0,0 +1,33 @@
+/*
+ * TreatJS: Sandbox 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014-2015, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+
+function Rule(effect, predicate) {
+  if(!(this instanceof Rule)) return new Rule(effect, predicate);
+
+  Object.defineProperties(this, {
+    "effect" : {
+      value: effect
+    },
+    "predicate": {
+      value: predicate
+    }
+  });
+}
+
+
+
+
+
+
